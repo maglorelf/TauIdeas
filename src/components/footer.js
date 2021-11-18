3f0cfd97ecd7337ffd000da2594f9dd2791c2c15
Marcos Javier Martínez Ramos <maglor_elf@yahoo.es>
2021-11-19 00:51:57 +0100

    Initial template

---
+++ src/components/footer.js
@@ -0,0 +1,109 @@
+import * as React from "react";
+import { Link } from "gatsby";
+
+import logo from "../images/tauideas.png";
+import twitter from "../images/social/twitter.svg";
+import linkedin from "../images/social/linkedin.svg";
+
+const Footer = class extends React.Component {
+  render() {
+    return (
+      <footer className="footer has-background-black has-text-white-ter">
+        <div className="content has-text-centered">
+          <img
+            src={logo}
+            alt="Tauideas"
+            style={{  }}
+          />
+        </div>
+        <div className="content has-text-centered has-background-black has-text-white-ter">
+          <div className="container has-background-black has-text-white-ter">
+            <div style={{ maxWidth: "100vw" }} className="columns">
+              <div className="column is-4">
+                <section className="menu">
+                  <ul className="menu-list">
+                    <li>
+                      <Link to="/" className="navbar-item">
+                        Home
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="navbar-item" to="/about">
+                        About
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="navbar-item" to="/products">
+                        Products
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="navbar-item" to="/contact/examples">
+                        Form Examples
+                      </Link>
+                    </li>
+                    <li>
+                      <a
+                        className="navbar-item"
+                        href="/admin/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Admin
+                      </a>
+                    </li>
+                  </ul>
+                </section>
+              </div>
+              <div className="column is-4">
+                <section>
+                  <ul className="menu-list">
+                    <li>
+                      <Link className="navbar-item" to="/blog">
+                        Latest Stories
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="navbar-item" to="/contact">
+                        Contact
+                      </Link>
+                    </li>
+                  </ul>
+                </section>
+              </div>
+              <div className="column is-4 social">
+              
+                <a title="twitter" href="https://twitter.com">
+                  <img
+                    className="fas fa-lg"
+                    src={twitter}
+                    alt="Twitter"
+                    style={{ width: "1em", height: "1em" }}
+                  />
+                </a>
+                <a title="linkedin" href="https://linkedin.com">
+                  <img
+                    className="fas fa-lg"
+                    src={linkedin}
+                    alt="Linkedin"
+                    style={{ width: "1em", height: "1em" }}
+                  />
+                </a>
+               
+              </div>
+            </div>
+           <div>
+           <div className="content has-text-left">
+        © {new Date().getFullYear()}, Marcos J. Martínez Ramos
+        {` `}
+        
+           </div>
+           </div>
+          </div>
+        </div>
+      </footer>
+    );
+  }
+};
+
+export default Footer;
---
+++ src/components/layout.js
@@ -0,0 +1,37 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import Footer from "./footer";
+import Navbar from "./navbar";
+import { withPrefix } from "gatsby";
+import "./all.sass";
+const Layout = ({ location, title, children }) => {
+  const rootPath = `${__PATH_PREFIX__}/`
+  const isRootPath = location.pathname === rootPath
+  let header
+
+  if (isRootPath) {
+    header = (
+      <h1 className="main-heading">
+        <Link to="/">{title}</Link>
+      </h1>
+    )
+  } else {
+    header = (
+      <Link className="header-link-home" to="/">
+        {title}
+      </Link>
+    )
+  }
+
+  return (
+    <div className="global-wrapper" data-is-root-path={isRootPath}>
+      <header className="global-header">{header}</header>
+<Navbar />
+      <main>{children}</main>
+     
+   <Footer />
+    </div>
+  )
+}
+
+export default Layout
