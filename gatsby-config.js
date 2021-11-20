172d4345987d3dc77232c6ea7394b6d5804552b0
Marcos Javier Martínez Ramos <maglor_elf@yahoo.es>
2021-11-20 01:39:07 +0100

    About page with markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,13 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        path: `${__dirname}/src/pages`,
+        name: `pages`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
