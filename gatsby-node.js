70aefbdc6ca49de62d53689a90e522ea2aedd8d8
Marcos Javier Martínez Ramos <maglor_elf@yahoo.es>
2021-12-01 00:42:06 +0100

    Partial changes to separate blogs and bascis pages #1

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,62 +4,51 @@ const { createFilePath } = require(`gatsby-source-filesystem`)
 exports.createPages = async ({ graphql, actions, reporter }) => {
   const { createPage } = actions
 
-  // Define a template for blog post
-  const blogPost = path.resolve(`./src/templates/blog-post.js`)
- // Define a template for basic pages
- const basicPageTemplate = path.resolve(`./src/templates/basic-page.js`)
   // Get all markdown blog posts sorted by date
   const result = await graphql(
     `
-      {
-        allMarkdownRemark(
-          sort: { fields: [frontmatter___date], order: ASC }
-          
-          limit: 1000
-        ) {
-          nodes {
+    {
+      allMarkdownRemark(sort: {fields: [frontmatter___date], order: ASC}, limit: 1000) {
+        edges {
+          node {
             id
             fields {
               slug
             }
+            frontmatter {
+              templateKey
+            }
           }
         }
       }
+    }
     `
   )
 
   if (result.errors) {
     reporter.panicOnBuild(
-      `There was an error loading your blog posts`,
+      `There was an error loading your posts`,
       result.errors
     )
     return
   }
 
-  const posts = result.data.allMarkdownRemark.nodes
+  const edges = result.data.allMarkdownRemark.edges
 
-  // Create blog posts pages
-  // But only if there's at least one markdown file found at "content/blog" (defined in gatsby-config.js)
-  // `context` is available in the template as a prop and as a variable in GraphQL
-
-  if (posts.length > 0) {
-    posts.forEach((post, index) => {
-      const previousPostId = index === 0 ? null : posts[index - 1].id
-      const nextPostId = index === posts.length - 1 ? null : posts[index + 1].id
-
-  createPage({
-    path: post.fields.slug,
-    component: blogPost,
-    context: {
-      id: post.id,
-      previousPostId,
-      nextPostId,
-    },
-  })
-
+  edges.forEach((edge) => {
+    const id = edge.node.id
+    createPage({
+      path: edge.node.fields.slug,      
+      component: path.resolve(`src/templates/${String(edge.node.frontmatter.templateKey)}.js`
+      ),
+      // additional data can be passed via context
+      context: {
+        id,
+      },
     })
+  })   
   }
-}
+
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
